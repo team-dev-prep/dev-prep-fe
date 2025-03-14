2653ed78517902253302cd69ea2c48f9e8405dd0
00kang <kdb070970@gmail.com>
2025-03-15 03:21:42 +0900

    feat: 타이머, 질문, 답변입력필드, 현재페이지번호, 버튼으로 구성된 InterView 화면 구현

--- src/pages/InterView.tsx
+++ src/pages/InterView.tsx
@@ -1,5 +1,35 @@
+import Timer from "../components/common/Timer";
+import Button from "../components/common/Button";
+import Counter from "../components/common/Counter";
+import Question from "../components/common/Question";
+import AnswerInput from "../components/common/AnswerInput";
+
 const Interview = () => {
-  return <h1 className="text-2xl font-bold text-gray-800">Interview Page</h1>;
+  return (
+    <div
+      className="mx-auto flex w-full max-w-[1200px] flex-col"
+      style={{ height: "calc(100vh - 130px)" }}
+    >
+      <div className="flex w-full justify-center py-6">
+        <Timer />
+      </div>
+
+      <div className="flex w-full max-w-[1200px] flex-1 flex-col px-4">
+        <Question text="타입스크립트의 infer 키워드에 대해서 설명해주세요." />
+        <div className="flex flex-1">
+          <AnswerInput />
+        </div>
+      </div>
+
+      <div className="flex w-full max-w-[1200px] items-center justify-between p-4 pt-6">
+        <div className="flex-1"></div>
+        <Counter current={1} total={1} />
+        <div className="flex flex-1 justify-end">
+          <Button to="/feedback" label="결과 보기" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Interview;
