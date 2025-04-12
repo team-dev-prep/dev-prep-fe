ea7a7d0c55854018460d85b692114efd2b8f8e68
00kang <kdb070970@gmail.com>
2025-04-12 20:48:55 +0900

    feat: LandingPage 라우트 추가 및 InterviewSetupPage 경로 "/option"으로 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import { FeedbackPage, InterviewPage, InterviewSetupPage } from "./pages";
+import { FeedbackPage, InterviewPage, InterviewSetupPage, LandingPage } from "./pages";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<InterviewSetupPage />} />
-          <Route path="/interview" element={<InterviewPage />} />
-          <Route path="/feedback" element={<FeedbackPage />} />
+          <Route index element={<LandingPage />} />
+          <Route path="option" element={<InterviewSetupPage />} />
+          <Route path="interview" element={<InterviewPage />} />
+          <Route path="feedback" element={<FeedbackPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
