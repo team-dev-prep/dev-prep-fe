7622cab57d50d7bcf8f4124959a1926286fd28db
00kang <kdb070970@gmail.com>
2025-04-12 21:49:44 +0900

    refactor: Button 컴포넌트에 children props 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,16 +1,17 @@
 interface ButtonProps {
-  label: string;
+  label?: string;
+  children?: React.ReactNode;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   className?: string;
 }
 
-const Button = ({ label, onClick, className }: ButtonProps) => {
+const Button = ({ label, children, onClick, className }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
       className={`${className} rounded-lg px-6 py-2 transition-transform duration-200 hover:scale-105`}
     >
-      {label}
+      {children ?? label}
     </button>
   );
 };
