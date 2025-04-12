16e296ae5d04dd95c9da354d0e55f52f0effd9ab
00kang <kdb070970@gmail.com>
2025-04-12 21:52:06 +0900

    feat: Header 컴포넌트에 로그인 버튼 및 UI 구조 추가
    - isLogin 상태 도입 및 로그인/로그아웃 분기 처리
    - UserCircle 아이콘 및 프로필 이미지 기반 UI 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "../common";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
   const navigate = useNavigate();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <header className="flex h-[70px] w-full items-center justify-start border-b border-solid border-gray2">
-      <button onClick={() => navigate("/")} className="px-4 text-xl font-black">
-        DevPrep
-      </button>
+    <header className="flex h-[70px] w-full items-center justify-between border-b border-solid border-gray2">
+      {/* 로고 */}
+      <Button label={"DevPrep"} onClick={() => navigate("/")} className="px-4 text-xl font-black" />
+
+      {/* 로그인/로그아웃 영역 */}
+      <div>
+        {isLogin ? (
+          <Button onClick={() => {}} className="hover:bg-blue2 flex items-center gap-1 text-gray8">
+            <UserCircleIcon className="size-[28px]" />
+            <p className="font-semibold">Login</p>
+          </Button>
+        ) : (
+          <Button onClick={() => {}} className="hover:bg-blue2 flex items-center gap-1 text-gray8">
+            <img src="" alt="프로필" className="size-[28px] rounded-full" />
+            <p>Logout</p>
+          </Button>
+        )}
+      </div>
     </header>
   );
 };
