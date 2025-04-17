dd99512b01e6368799f83b0162a9197cc5150fee
00kang <kdb070970@gmail.com>
2025-04-17 12:39:09 +0900

    feat: GitHub OAuth 로그인 버튼 구현 및 리디렉션 처리

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,8 +5,15 @@ import { UserCircleIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
   const navigate = useNavigate();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isLogin, setIsLogin] = useState(true);
+
+  const handleGithubLogin = () => {
+    const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_GITHUB_REDIRECT_URI;
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user user:email`;
+
+    window.location.href = githubAuthUrl;
+  };
 
   return (
     <header className="flex h-[70px] w-full items-center justify-between border-b border-solid border-gray2">
@@ -16,12 +23,15 @@ const Header = () => {
       {/* 로그인/로그아웃 영역 */}
       <div>
         {isLogin ? (
-          <Button onClick={() => {}} className="hover:bg-blue2 flex items-center gap-1 text-gray8">
+          <Button
+            onClick={handleGithubLogin}
+            className="flex items-center gap-1 text-gray8 hover:bg-blue2"
+          >
             <UserCircleIcon className="size-[28px]" />
             <p className="font-semibold">Login</p>
           </Button>
         ) : (
-          <Button onClick={() => {}} className="hover:bg-blue2 flex items-center gap-1 text-gray8">
+          <Button onClick={() => {}} className="flex items-center gap-1 text-gray8 hover:bg-blue2">
             <img src="" alt="프로필" className="size-[28px] rounded-full" />
             <p className="font-semibold">Logout</p>
           </Button>
