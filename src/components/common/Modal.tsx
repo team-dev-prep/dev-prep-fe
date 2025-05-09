6cd8cc4bd2bdcb72bd56e4ccf2f47362e4692eda
00kang <kdb070970@gmail.com>
2025-05-09 17:03:11 +0900

    feat: 공통 Modal 컴포넌트 구현 및 Portal 방식 적용
    - createPortal로 modal-root에 렌더링
    - 모달 열릴 때 body 스크롤 잠금 처리

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  content?: React.ReactNode;
+  actions?: React.ReactNode;
+}
+
+const Modal = ({ isOpen, onClose, title, content, actions }: ModalProps) => {
+  const [mounted, setMounted] = useState(false); // 클라이언트 렌더링 여부
+
+  // 클라이언트에서만 모달 렌더링
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  // 모달 열릴 때 스크롤 비활성화
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  if (!isOpen || !mounted) return null;
+
+  const portalRoot = document.getElementById("modal-root");
+  if (!portalRoot) {
+    console.error("modal-root가 HTML에 없습니다.");
+    return null;
+  }
+
+  return createPortal(
+    <div
+      className="bg-black-60 fixed inset-0 z-50 flex items-center justify-center"
+      onClick={onClose}
+    >
+      <div
+        className="flex flex-col rounded-lg bg-white p-6 shadow-lg"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {/* 닫기 버튼*/}
+        <div className="flex justify-end">
+          <button onClick={onClose} className="cursor w-fit text-sm text-gray8 hover:underline">
+            닫기
+          </button>
+        </div>
+
+        {/* 내용 영역 */}
+        <div className="flex flex-col gap-6">
+          {title && <h2 className="text-2xl font-bold text-black">{title}</h2>}
+          {content && <div className="text-lg text-gray8">{content}</div>}
+          {actions && <div className="flex flex-col gap-3 text-lg">{actions}</div>}
+        </div>
+      </div>
+    </div>,
+    portalRoot,
+  );
+};
+
+export default Modal;
